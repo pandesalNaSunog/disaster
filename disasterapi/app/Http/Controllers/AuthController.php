5d03427be56e9e61d8a381cb1ce81f8290f125a7
pandesalNaSunog <floresjem8@gmail.com>
2022-10-03 15:49:30 +0800

    latest

--- disasterapi/app/Http/Controllers/AuthController.php
+++ disasterapi/app/Http/Controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
 
         $mail = new PHPMailer(true);
 
-        $mail->SMTPDebug = 4;
+        $mail->SMTPDebug = 0;
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
