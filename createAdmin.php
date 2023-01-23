f60e7940c4925f5c10799b8b7d629ef5b8989ba9
pandesalNaSunog <floresjem8@gmail.com>
2023-01-24 01:12:38 +0800

    latest

--- createAdmin.php
+++ createAdmin.php
@@ -1,6 +1,6 @@
 <?php
-    //$con = new mysqli("localhost","u568496919_disaster","DisasterPassword11","u568496919_disaster_db");
-    $con = new mysqli("localhost","root","","disaster_db");
+    $con = new mysqli("localhost","u568496919_disaster","DisasterPassword11","u568496919_disaster_db");
+    //$con = new mysqli("localhost","root","","disaster_db");
 
     $password = password_hash('password', PASSWORD_DEFAULT);
     $query = "INSERT INTO users(`email`,`password`,`user_type`)VALUES('user@admin.com','$password','admin')";
