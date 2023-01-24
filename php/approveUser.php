664277370a44e073f7f96c460e5f3b57a878cdc6
pandesalNaSunog <floresjem8@gmail.com>
2023-01-24 16:30:00 +0800

    latest

--- php/approveUser.php
+++ php/approveUser.php
@@ -1,4 +1,9 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/autoload.php';
     include('server.php');
     if(secured()){
         include('connection.php');
@@ -7,9 +12,39 @@
             $userId = htmlspecialchars($_POST['user_id']);
             $query = "UPDATE users SET approved = 'yes' WHERE id = '$userId'";
             $con->query($query) or die($con->error);
+
+            $query = "SELECT * FROM users WHERE id = '$userId'";
+            $result = $con->query($query);
+            $user = $result->fetch_assoc();
+            $email = $user['email'];
+            sendMail($con, $email);
             echo 'ok';
         }
     }else{
         showError();
+    }
+
+    function sendMail($con, $email){
+
+        $query = "SELECT * FROM creds";
+        $result = $con->query($query);
+        $creds = $result->fetch_assoc();
+        $mail = new PHPMailer(true);
+
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $creds['email']; 
+        $mail->Password = $creds['key']; 
+        $mail->SMTPSecure = $creds['secure']; 
+        $mail->Port = $creds['port'] ;
+
+        $mail->setFrom($creds['email'], 'Exodus Search and Rescue, Inc.');
+        $mail->addAddress($email);
+        $mail->isHTML(true);
+
+        $mail->Subject = 'One Time Password';
+        $mail->Body = 'Your account has been approved by the administrator. You may now proceed to log in.';
     }
 ?>
---
+++ php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '83ec9bd9cc2a98974b8e7e750bbe7fc4718e4a2e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '83ec9bd9cc2a98974b8e7e750bbe7fc4718e4a2e',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.7.1',
+            'version' => '6.7.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '49cd7ea3d2563f028d7811f06864a53b1f15ff55',
+            'dev_requirement' => false,
+        ),
+    ),
+);
