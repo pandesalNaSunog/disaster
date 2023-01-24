fc2347b6e612c6d6b453cd7fde54887b1940bfb9
pandesalNaSunog <floresjem8@gmail.com>
2023-01-24 16:53:48 +0800

    latest

--- php/approveUser.php
+++ php/approveUser.php
@@ -1,11 +1,43 @@
 <?php
+    require 'vendor/PHPMailer/PHPMailer/src/Exception.php';
+    require 'vendor/PHPMailer/PHPMailer/src/PHPMailer.php';
+    require 'vendor/PHPMailer/PHPMailer/src/SMTP.php';
+    require 'vendor/autoload.php';
+
+
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'vendor/autoload.php';
+    
     include('server.php');
     if(secured()){
+        function sendMail($con, $email){
+
+            $query = "SELECT * FROM creds";
+            $result = $con->query($query);
+            $creds = $result->fetch_assoc();
+            $mail = new PHPMailer(true);
+    
+            $mail->SMTPDebug = 2;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = $creds['email']; 
+            $mail->Password = $creds['key']; 
+            $mail->SMTPSecure = $creds['secure']; 
+            $mail->Port = $creds['port'] ;
+    
+            $mail->setFrom($creds['email'], 'Exodus Search and Rescue, Inc.');
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+    
+            $mail->Subject = 'Account Approved';
+            $mail->Body = 'Your account has been approved by the administrator. You may now proceed to log in.';
+            $mail->send();
+            echo json_encode(array('creds' => $creds, 'email' => $email));
+        }
+
         include('connection.php');
         $con = connect();
         if(isset($_POST['user_id'])){
@@ -18,33 +50,12 @@
             $user = $result->fetch_assoc();
             $email = $user['email'];
             sendMail($con, $email);
+            
             echo 'ok';
         }
     }else{
         showError();
     }
 
-    function sendMail($con, $email){
-
-        $query = "SELECT * FROM creds";
-        $result = $con->query($query);
-        $creds = $result->fetch_assoc();
-        $mail = new PHPMailer(true);
-
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = $creds['email']; 
-        $mail->Password = $creds['key']; 
-        $mail->SMTPSecure = $creds['secure']; 
-        $mail->Port = $creds['port'] ;
-
-        $mail->setFrom($creds['email'], 'Exodus Search and Rescue, Inc.');
-        $mail->addAddress($email);
-        $mail->isHTML(true);
-
-        $mail->Subject = 'One Time Password';
-        $mail->Body = 'Your account has been approved by the administrator. You may now proceed to log in.';
-    }
+    
 ?>
