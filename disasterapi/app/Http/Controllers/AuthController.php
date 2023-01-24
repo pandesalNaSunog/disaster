1658f9b8c73c16c6ba54c5977e1d701fa9618108
pandesalNaSunog <floresjem8@gmail.com>
2023-01-24 18:53:59 +0800

    latest

--- disasterapi/app/Http/Controllers/AuthController.php
+++ disasterapi/app/Http/Controllers/AuthController.php
@@ -126,6 +126,31 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('myToken')->plainTextToken;
+        $creds = Cred::where('id', 1)->first();
+        $mail = new PHPMailer(true);
+
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $creds->email; 
+        $mail->Password = $creds->key; 
+        $mail->SMTPSecure = $creds->secure; 
+        $mail->Port = $creds->port ;
+
+        $mail->setFrom($creds->email, 'Exodus Search and Rescue, Inc.');
+        $mail->addAddress($request['email']);
+        $mail->isHTML(true);
+
+        $mail->Subject = 'One Time Password';
+        $mail->Body = 'Your OTP is ' . $otp . ". Note that this can only be used once. Do not close the app or this information will not be valid anymore.";
+
+        if(!$mail->send()){
+            $user->delete();
+            return response ([
+                'message' => 'email is invalid'
+            ], 401);
+        }
 
         return response([
             'token' => $token
